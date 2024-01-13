96bf0f56b226fb6c7df3243d2460a2149f694036
DamitusThyYeetus123 <108782125+DamitusThyYeetus123@users.noreply.github.com>
2024-01-13 23:14:33 +1100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import Pronoun from './components/Dependent/Pronoun';
 
 const Patcher = create("pronoun-db")
 
-const UserProfile = getModule(=> x?.type?.name == "UserProfile")
+const UserProfile = getModule(x => x?.type?.name == "UserProfile")
 const UserStore = getByProps("getUser");
 const ReactNative = getByProps("View") as typeof import("react-native");
 const { DCDChatManager } = ReactNative.NativeModules;
